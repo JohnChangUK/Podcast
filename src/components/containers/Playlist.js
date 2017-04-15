da0325a6cc9bd3fc4d89c28bd84f586683147230
John Chang <johnchang9094@gmail.com>
2017-04-15 22:06:48 +0100

    Added Playlist Container, which renders the Search Component

---
+++ src/components/containers/Playlist.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Search } from '../presentation';
+
+class Playlist extends Component { 
+
+    render() {
+        return (
+            <div>
+                <div className="hero-header bg-shop animated fadeindown">
+                    <h1 className="hero-title">Playlist</h1>
+                </div>      
+                
+                <Search />
+            </div>      
+            );
+    }
+}
+
+export default Playlist;
