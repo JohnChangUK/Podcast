135685e36723f2dbb3f7b2d32eecb9259b71bc14
John Chang <johnchang9094@gmail.com>
2017-04-15 13:07:24 +0100

    Setup superagent AJAX api requests, iTunes search API

---
+++ routes/search.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+var superagent = require('superagent');
+
+/* GET users listing. */
+router.get('/:term', function(req, res, next) {
+  var term = req.params.term;
+
+// request iTunes search API
+  var url = 'http://itunes.apple.com/search';
+
+  superagent
+  .get(url)
+  .query({media: 'podcast', term: term})
+  .set('Accept', 'application/json')
+  .end(function(err, response) {
+    if (err) {
+      res.json({
+      confirmation: 'Fail',
+      message: err
+    });
+
+      return;
+    }
+
+    console.log(JSON.stringify(response));
+
+    var data = JSON.parse(response.text);
+
+      res.json({
+      confirmation: 'success',
+      response: data.results
+    });
+
+  });
+
+});
+
+module.exports = router;
