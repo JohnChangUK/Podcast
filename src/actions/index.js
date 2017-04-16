32f763ce4498522a36d32126d400233a602edcec
John Chang <johnchang9094@gmail.com>
2017-04-16 20:48:09 +0100

    Added All Actions and Action Creators

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import constants from '../constants';
+
+export default { 
+
+    searchPodcasts: (params) => {
+        console.log('searchPodcasts: ' + JSON.stringify(params))
+
+    },
+
+    podcastsReceived: (podcasts) => {
+        return {
+            type: constants.PODCASTS_RECEIVED,
+            podcasts: podcasts
+        };
+    }
+};
