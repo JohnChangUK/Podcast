1a603b4e3a556bf0104e37bb18b866aaced0f952
John Chang <johnchang9094@gmail.com>
2017-04-16 20:49:27 +0100

    Implemented podcast Reducer, switch statement with (action.type)

---
+++ src/reducers/podcastReducer.js
@@ -0,0 +1,20 @@
+import constants from '../constants';
+
+var initialState = {
+    all: null
+};
+
+export default (state = initialState, action) => {
+    let updated = Object.assign( {}, state);
+
+    switch(action.type) {
+        case constants.PODCASTS_RECEIVED:
+            console.log('PODCASTS_RECEIVED: ' + JSON.stringify(action.podcasts));
+            updated['all'] = action.podcasts;
+
+            return updated;
+
+        default:
+            return state;
+    }
+};
