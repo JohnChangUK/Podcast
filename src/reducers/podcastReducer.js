8e1f0354a7084f4277533079d812c48dfe40eba6
John Chang <johnchang9094@gmail.com>
2017-04-17 14:47:28 +0100

    Added action.type 'constants.PODCAST_SELECTED' and updated the state

--- src/reducers/podcastReducer.js
+++ src/reducers/podcastReducer.js
@@ -1,18 +1,30 @@
 import constants from '../constants';
 
 var initialState = {
-    all: null
+    all: null,
+    selected: null
 };
 
+// Reducers are pieces of data to be saved in the Store
 export default (state = initialState, action) => {
-    let updated = Object.assign( {}, state);
+    let updated = Object.assign({}, state);
 
     switch(action.type) {
         case constants.PODCASTS_RECEIVED:
-            console.log('PODCASTS_RECEIVED: ' + JSON.stringify(action.podcasts));
+            // console.log('PODCASTS_RECEIVED: ' + JSON.stringify(action.podcasts));
             updated['all'] = action.podcasts;
 
             return updated;
+
+        case constants.PODCAST_SELECTED:
+        // console.log('PODCAST_SELECTED: ' + JSON.stringify(action.podcast));
+        if (updated.selected != null) {
+            if (updated.selected.collectionId == action.podcast.collectionId)
+                return state;
+        }
+
+        updated['selected'] = action.podcast;
+        return updated;
 
         default:
             return state;
